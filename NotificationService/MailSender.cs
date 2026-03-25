084e90dd49f682667458e7f0df830e5e3bc64fa1
DehliFin <dehli1212@gmail.com>
2026-03-25 18:00:37 +0100

    gg

---
+++ NotificationService/MailSender.cs
@@ -0,0 +1,48 @@
+﻿using MailKit.Security;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+
+
+namespace Notification
+{
+    public class MailSender
+    {
+        private string smtpHost = "smtp-relay.brevo.com";
+        private int smtpPort = 587;
+        private string smtpUsername = "a5de95001@smtp-brevo.com";
+        private string smtpPassword = "QLxw2d8NMjbSOPYc";
+
+
+        public async Task SendEmailAsync(
+            string fromEmail,
+            string fromName,
+            string toEmail,
+            string subject,
+            string body)
+        {
+            var email = new MimeMessage();
+
+            email.From.Add(new MailboxAddress(fromName, fromEmail));
+            email.To.Add(MailboxAddress.Parse(toEmail));
+            email.Subject = subject;
+
+            email.Body = new TextPart("html")
+            {
+                Text = body
+            };
+
+            using var smtp = new SmtpClient();
+
+            await smtp.ConnectAsync(smtpHost, smtpPort, SecureSocketOptions.StartTls);
+            await smtp.AuthenticateAsync(smtpUsername, smtpPassword);
+            await smtp.SendAsync(email);
+            await smtp.DisconnectAsync(true);
+        }
+    }
+    
+}
---
+++ NotificationService/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using Notification;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Starter notification service...");
+
+       
+
+        string fromEmail = "jeso0005@stud.ek.dk";
+        string fromName = "School Notification Service";
+
+        string toEmail = "dehli11111@gmail.com";
+        string subject = "Test notifikation";
+        string body = "<h2>Hej</h2><p>Dette er en testmail fra min C# console notification app.</p>";
+
+        MailSender mailSender = new MailSender();
+
+        try
+        {
+            await mailSender.SendEmailAsync(
+                fromEmail,
+                fromName,
+                toEmail,
+                subject,
+                body
+            );
+
+            Console.WriteLine("Mail sendt!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Fejl ved afsendelse:");
+            Console.WriteLine(ex.Message);
+        }
+
+        Console.WriteLine("Tryk på en tast for at afslutte...");
+        Console.ReadKey();
+    }
+}
