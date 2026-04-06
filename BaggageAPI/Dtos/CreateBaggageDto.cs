92d41eac1cec5da7856298bcea6272e97d4d7f2e
Nikolai <119046653+Nroenman@users.noreply.github.com>
2026-04-06 10:14:20 +0200

    .

---
+++ BaggageAPI/Dtos/CreateBaggageDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BaggageAPI.Dtos;
+
+public class CreateBaggageDto
+{
+    public Guid BookingId { get; set; }
+    public Guid PassengerId { get; set; }
+    public double Weight { get; set; }
+}
---
+++ BaggageAPI/Services/BaggageService.cs
@@ -0,0 +1,55 @@
+﻿using BaggageAPI.Data;
+using BaggageAPI.Dtos;
+using BaggageAPI.Interfaces;
+using BaggageAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BaggageAPI.Services;
+
+public class BaggageService(AppDbContext context) : IBaggageService
+{
+    private readonly AppDbContext _context = context;
+
+    public async Task<Baggage> CreateAsync(CreateBaggageDto dto)
+    {
+        var baggage = new Baggage
+        {
+            Id = Guid.NewGuid(),
+            BookingId = dto.BookingId,
+            PassengerId = dto.PassengerId,
+            Weight = dto.Weight,
+            Status = BaggageStatus.CheckedIn,
+            CurrentLocation = "Check-in",
+            CreatedAt = DateTime.UtcNow
+        };
+
+        _context.Baggages.Add(baggage);
+        await _context.SaveChangesAsync();
+
+        //  Send event via RabbitMQ
+        // "BaggageCheckedIn"
+
+        return baggage;
+    }
+
+    public async Task<Baggage> UpdateStatusAsync(Guid id, UpdateBaggageStatusDto dto)
+    {
+        var baggage = await _context.Baggages.FindAsync(id);
+
+        baggage.Status = dto.Status;
+        baggage.CurrentLocation = dto.Location;
+
+        await _context.SaveChangesAsync();
+
+        // 🔔 Publish event: BaggageStatusUpdated
+
+        return baggage;
+    }
+
+    public async Task<List<Baggage>> GetByPassenger(Guid passengerId)
+    {
+        return await _context.Baggages
+            .Where(x => x.PassengerId == passengerId)
+            .ToListAsync();
+    }
+}
