182575bbad6d137e3b1c85685df39197d4155abb
Muhammad Ali Khan <mak_121212@hotmail.com>
2026-04-24 16:34:47 +0200

    payment with stripe  is completed

--- PaymentService/routes/paymentRoutes.js
+++ PaymentService/routes/paymentRoutes.js
@@ -3,11 +3,10 @@ const router = express.Router();
 const paymentController = require("../controllers/paymentController");
 
 router.post('/stripe/checkout', paymentController.stripe.checkout);
-router.post("/stripe/webhook", express.raw({ type: "application/json" }), paymentController.stripe.webhook);
 
 router.post("/stripe/payment", paymentController.stripe.payment);
 
-router.get('/stripe/success', paymentController.stripe.success);
-router.get('/stripe/cancel', paymentController.stripe.cancel);
+router.get('/stripe/success/:flightID', paymentController.stripe.success);
+router.get('/stripe/cancel/:flightID', paymentController.stripe.cancel);
 
 module.exports = router;
