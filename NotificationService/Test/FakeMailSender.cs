7a1022f37ec4166d8e826dc852e36d641c402d76
DehliFin <42273903+DehliFin@users.noreply.github.com>
2026-04-28 12:20:55 +0200

    Test

---
+++ NotificationService/Test/FakeMailSender.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notification.Test
+{
+    public class FakeMailSender : IMailSender
+    {
+        public List<NotificationMessage> SentMessages { get; } = new();
+
+        public Task SendEmailAsync(
+            string fromEmail,
+            string fromName,
+            string toEmail,
+            string subject,
+            string body)
+        {
+            SentMessages.Add(new NotificationMessage
+            {
+                FromName = fromName,
+                ToEmail = toEmail,
+                Subject = subject,
+                Body = body
+            });
+
+            return Task.CompletedTask;
+        }
+        public Task SendEmailAsyncfail(
+        string fromEmail,
+        string fromName,
+        string toEmail,
+        string subject,
+        string body)
+        {
+            throw new Exception("Mail fejlede");
+        }
+    }
+}
